3327b0d9dd20ab907c423a10427401103fac4318
Nurlan949 <nurlanali246@gmail.com>
2026-06-18 11:08:59 +0400

    file upload

---
+++ src/test/java/com/project/metapostperformanceanalysis/MetaPostPerformanceAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.metapostperformanceanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaPostPerformanceAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
